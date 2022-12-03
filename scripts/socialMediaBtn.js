6a8be15ffe76976beaf79fe1c431478948e5997e
Maera <baeitsmae@gmail.com>
2022-12-03 21:23:16 +0100

    social medias buttons added

---
+++ scripts/socialMediaBtn.js
@@ -0,0 +1,35 @@
+const socialMediaSection = document.querySelector(".social-media");
+
+const socialBtn = document.createElement("div");
+const linkedinBtn = document.createElement("div");
+const linkedinImg = document.createElement("img");
+const githubBtn = document.createElement("div");
+const githubImg = document.createElement("img");
+
+socialBtn.classList.add("social-btn");
+linkedinBtn.classList.add("linkedin");
+githubBtn.classList.add("github");
+
+linkedinImg.setAttribute("src", "../assets/icons/linkedin.png");
+githubImg.setAttribute("src", "../assets/icons/github.png");
+
+
+socialBtn.addEventListener("click", () => {
+    socialBtn.classList.toggle("social-btn-active");
+    linkedinBtn.classList.toggle("linkedin-active");
+    githubBtn.classList.toggle("github-active");
+})
+
+linkedinBtn.addEventListener("click", () =>{
+    window.open("https://www.linkedin.com/in/maera-contaret-5ab94a221/")
+})
+
+githubBtn.addEventListener("click", () =>{
+    window.open("https://github.com/MaeraC")
+})
+
+socialMediaSection.appendChild(socialBtn);
+socialMediaSection.appendChild(linkedinBtn);
+linkedinBtn.appendChild(linkedinImg);
+socialMediaSection.appendChild(githubBtn);
+githubBtn.appendChild(githubImg);
